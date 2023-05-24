a98ced066886d850939deeb9bd097d5622d1ed2b
Guy-Merlin FOTSO <129025129+fgm237@users.noreply.github.com>
2023-05-24 16:41:48 +0100

    Update microapp.js

--- microapp.js
+++ microapp.js
@@ -1,29 +1,3 @@
-var Ayoba = getAyoba()
- 
-/**
-* Determine the mobile operating system and returns the
-* proper javascript interface
-*/
-function getAyoba() {
-   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
- 
-   // Windows Phone must come first because its UA also contains "Android"
-   if (/windows phone/i.test(userAgent)) {
-       return null;
-   }
- 
-   if (/android/i.test(userAgent)) {
-       return Android;
-   }
- 
-   // iOS detection from: http://stackoverflow.com/a/9039885/177710
-   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-       return null; // todo
-   }
- 
-   return "unknown";
-}
-
 window.fcWidget.init({
     token: "ce73e275-a505-4d32-b8ab-dae656c3f258",
     host: "https://wchat.freshchat.com" ,
